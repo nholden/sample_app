bbe91cee7db7f646724a3ef395b13a576a1b7835
Nick Holden <nholden@bu.edu>
2014-07-31 07:20:50 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -60,6 +60,12 @@ describe "User pages" do
         before { click_button submit }
         it { should have_title('Example User') }
         it { should have_content('Welcome to the Sample App!') }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome to the Sample App!') }
+
+        describe "followed by signout" do
+          before { click_link "Sign out" }
+          it { should have_link('Sign in') }
+        end
       end
     end
   end
